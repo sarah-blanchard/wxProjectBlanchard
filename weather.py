b11d51f278d2d35f0a6bbb157e44429867d78879
sarah-blanchard <71983673+sarah-blanchard@users.noreply.github.com>
2020-10-09 21:23:14 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -23,102 +23,68 @@ def getZipCode():
 		
 	return zipcode
 
-zipcode = getZipCode()
 
 def create_url():
 	full_url = ""
 	url = "http://api.openweathermap.org/data/2.5/weather?zip="
+	zipcode = getZipCode()
 	api = "e4f9c3989f6e3d0f337d8f18c8995bef"
 	
 	full_url = url + zipcode + "&units=imperial&us&appid=" + api
 
 	return full_url
 
-full_url = create_url()
 
-create_url()
+def get_weather():
+	full_url = create_url()
+	req = requests.get(full_url)
+	wx = req.json()
 
-req = requests.get(full_url)
-wx = req.json()
-
-
-
-if wx["cod"] == "200": 
+	if wx["cod"] != "404": 
     
-    location_name = wx["name"]
+		location_name = wx["name"]
 
-    tempDetails = wx["main"]
-    current_temperature = tempDetails["temp"] 
-    current_humidiy = tempDetails["humidity"] 
-    current_pressure = tempDetails["pressure"] 
+		tempDetails = wx["main"]
+		current_temperature = tempDetails["temp"] 
+		current_humidiy = tempDetails["humidity"] 
+		current_pressure = tempDetails["pressure"] 
 
-    windDetails = wx["wind"]
-    current_wind = windDetails["speed"]
+		windDetails = wx["wind"]
+		current_wind = windDetails["speed"]
     
-    current_statement = wx["weather"] 
-    weather_description = current_statement[0]["description"] 
+		current_statement = wx["weather"] 
+		weather_description = current_statement[0]["description"] 
 
-    print("Current weather for " + location_name + ": " + str(weather_description) +
-        "\n Temperature: " + str(current_temperature) + degree_sign + "F" +
-    	"\n Humidity: " + str(current_humidiy) + "%" +
-    	"\n Wind: " + str(current_wind) + "mph" +
-		"\n Atmospheric Pressure: " + str(current_pressure) + " hPa")
-  
-else: 
-    print("An unexpected error has occured, please try again") 
-    show_Main_Menu()
+		print("Current weather for " + location_name + ": " + str(weather_description) +
+	    	"\n Temperature: " + str(current_temperature) + degree_sign + "F" +
+			"\n Humidity: " + str(current_humidiy) + "%" +
+			"\n Wind: " + str(current_wind) + "mph" +
+			"\n Atmospheric Pressure: " + str(current_pressure) + " hPa")
 
-
-
+	else: 
+		print("An unexpected error has occured, please ensure you have entered the correct zip code and try again") 
+		show_Main_Menu()
 
 
 def run_Weather():
-	garage = []
 	while (True):
 		menu_option = show_Main_Menu()
 
 		if (menu_option == 1):
-			if (len(garage) == 0):
-				print('Your garage is currently empty, please add a vehicle.')
-			else:
-				view_Garage(garage)
+			your_weather = get_weather()
+			print(your_weather)
 
-		elif (garage_option == 2):
-			Vehicle_Description = get_Car_Description()
-			car = Car(Vehicle_Description['make'],
-						Vehicle_Description['model'],
-						Vehicle_Description['color'],
-						Vehicle_Description['fuel_type'],
-						Vehicle_Description['options'],
-						Vehicle_Description['engine_size'],
-						Vehicle_Description['num_doors'])
-
-			garage.append(car)
-			print('This car has been added to your garage.')
-
-		elif (garage_option == 3):
-			Vehicle_Description = get_Truck_Description()
-			truck = Truck(Vehicle_Description['make'],
-						Vehicle_Description['model'],
-						Vehicle_Description['color'],
-						Vehicle_Description['fuel_type'],
-						Vehicle_Description['options'],
-						Vehicle_Description['cab_style'],
-						Vehicle_Description['bed_length'])
-
-			garage.append(truck)
-			print('This truck has been added to your garage.')
-
-		elif (garage_option == 4):
+		elif (menu_option == 2):
 			print('Come back soon.')
 			break
+		
 		else:
 			print('Please select a valid option')
 
-		input('Press enter to continue')
+		input('Press enter to return to the Menu')
 	
 
-run_Garage()#def run_weather
+run_Weather()
 
 #if connection successful, print wx
 #would you like to see the weather for another location?
