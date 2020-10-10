919e9c3db0ecff0385c2d65eb284803e5925295a
sarah-blanchard <71983673+sarah-blanchard@users.noreply.github.com>
2020-10-09 21:27:32 -0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,7 +1,15 @@
-
 import requests, json
 
 #welcome_message = "Welcome Message"
+
+degree_sign= u'\N{DEGREE SIGN}'
+
+def show_Main_Menu():
+	print('Welcome to WEATHER NOW')
+	print('1 - Find current weather for a specific location')
+	print('2 - Quit')
+
+	return int(input('What would you like to do? '))
 
 
 def getZipCode():
@@ -15,23 +23,72 @@ def getZipCode():
 		
 	return zipcode
 
-zipcode = getZipCode()
 
 def create_url():
 	full_url = ""
-	url = "api.openweathermap.org/data/2.5/forecast?zip="
+	url = "http://api.openweathermap.org/data/2.5/weather?zip="
+	zipcode = getZipCode()
 	api = "e4f9c3989f6e3d0f337d8f18c8995bef"
 	
-	full_url = url + zipcode + ",us&appid=" + api
+	full_url = url + zipcode + "&units=imperial&us&appid=" + api
 
 	return full_url
 
-full_url = create_url()
 
-create_url()
+def get_weather():
+	full_url = create_url()
+	req = requests.get(full_url)
+	wx = req.json()
 
-req = requests.get(full_url)
-wx = req.json()
+	if wx["cod"] != "404": 
+    
+		location_name = wx["name"]
+
+		tempDetails = wx["main"]
+		current_temperature = tempDetails["temp"] 
+		current_humidiy = tempDetails["humidity"] 
+		current_pressure = tempDetails["pressure"] 
+
+		windDetails = wx["wind"]
+		current_wind = windDetails["speed"]
+    
+		current_statement = wx["weather"] 
+		weather_description = current_statement[0]["description"] 
+
+		print("Current weather for " + location_name + ": " + str(weather_description) +
+	    	"\n Temperature: " + str(current_temperature) + degree_sign + "F" +
+			"\n Humidity: " + str(current_humidiy) + "%" +
+			"\n Wind: " + str(current_wind) + "mph" +
+			"\n Atmospheric Pressure: " + str(current_pressure) + " hPa")
+
+	else: 
+		print("An unexpected error has occured, please ensure you have entered the correct zip code and try again") 
+		show_Main_Menu()
+
+
+def run_Weather():
+	while (True):
+		menu_option = show_Main_Menu()
+
+		if (menu_option == 1):
+			your_weather = get_weather()
+			print(your_weather)
+
+		elif (menu_option == 2):
+			print('Come back soon.')
+			break
+		
+		else:
+			print('Please select a valid option')
+
+		input('Press enter to return to the Menu')
+	
+
+run_Weather()
 
 #if connection successful, print wx
+#would you like to see the weather for another location?
+#yes, loop, no, goodbye
 #if unsuccessful say try again, if still having erros, try later
+#loop to beginning
+
